41c11beb434eaf33e03d717e10615387a4c2e8ef
Satvik Nagpal <51298948+satviknagpal01@users.noreply.github.com>
2021-05-11 10:23:16 +0530

    new

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public float speed = 6f;
+    public float jumpspeed = 9f;
+    float i = 0, c=1, p=10, lambda=7, m;
+    Vector2 velocity;
+    private new Rigidbody2D rigidbody2D;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
+        var h = Input.GetAxis("Horizontal");
+        rigidbody2D.velocity = new Vector2(h * speed, rigidbody2D.velocity.y);
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody2D.velocity = new Vector2(rigidbody2D.velocity.x, jumpspeed);
+            m = rigidbody2D.gravityScale * 2 + lambda - p;
+            rigidbody2D.gravityScale = (lambda * c + m) % p;
+        }
         
     }
 }
---
+++ Assets/scripts/camerafollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camerafollow : MonoBehaviour
+{
+    public Transform target;
+
+  //  public float offsetX;
+    private void FixedUpdate()
+    {
+        float playerXPos = target.position.x;
+        float cameraXPos = Mathf.Clamp(playerXPos, 0f, 185f);
+        transform.position = new Vector3(cameraXPos, 0f, -10f);
+        
+    }
+}
